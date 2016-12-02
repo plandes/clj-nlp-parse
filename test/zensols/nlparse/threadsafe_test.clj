25455d9e713e78600be3a9c112ca2dfdf016e93f
Paul Landes <landes@mailc.net>
2016-12-02 15:45:59 -0600

    adding threadding test

---
+++ test/zensols/nlparse/threadsafe_test.clj
@@ -0,0 +1,21 @@
+(ns zensols.nlparse.threadsafe-test
+  (:require [clojure.test :refer :all])
+  (:require [clojure.string :as s]
+            [zensols.nlparse.parse :refer :all]))
+
+(deftest in-test
+  (testing "testing threadsafe"
+    (let [agent-count 500]
+      (is (= agent-count
+             (let [utterance "My name is Paul Landes"
+                   correct-res (parse utterance)
+                   agents (map (fn [_] (agent nil))
+                               (range agent-count))]
+               (doseq [a agents]
+                 (send a (fn [_]
+                           (parse utterance))))
+               (apply await agents)
+               (->> agents
+                    (map #(->> % deref (= correct-res)))
+                    (filter true?)
+                    count)))))))
