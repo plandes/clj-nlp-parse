34cba647fb04917d326a04dc45166a088f7a07ef
Paul Landes <landes@mailc.net>
2017-02-08 09:44:12 -0600

    build

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
   :plugins [[lein-codox "0.10.3"]
+            [lein-javadoc "0.3.0"]
             [org.clojars.cvillecsteele/lein-git-version "1.2.7"]]
   :codox {:metadata {:doc/format :markdown}
           :project {:name "NLP Parsing and Feature Creation"}
